755b8dc3622b73a1b564835605e73f4d3c72eab9
Carla <mcarlaalexia@gmail.com>
2022-12-02 17:17:22 -0300

    commit del viernes

--- src/Entidades/Clase1.java
+++ src/Entidades/Clase1.java
@@ -3,7 +3,70 @@ package Entidades;
 public class Clase1 {
 
     private String nombre;
+    private String apellido;
+    private String email;
+    private int contraseña;
     private int edad;
-    private int dni;
-    private int altura;
+
+    public Clase1() {
+    }
+
+    public Clase1(String nombre, String apellido, String email, int contraseña, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(int contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase1{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", contraseña=" + contraseña +
+                ", edad=" + edad +
+                '}';
+    }
 }
--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,6 +1,23 @@
 package Principal;
-
 public class Main {
+
+       //Lo que anote 🫣
+    //Crear una app bancaria que contenga:
+    //10 usuarios
+    //usuario sea mayor de edad para abrir una cuenta
+    //datos de usuario: Nombre, Apellido, mail, Contraseña(alfanumérica)
+    //El sistema debe generar un número único de tarjeta cuando se crea la cuenta.
+    //Menú para transacción de usuario 1 a usuario 2.
+
+    //* hasta 10 usuarios
+    // * controlar q nada llegue vacio
+    // * nombre ap direc tel correo
+    // * usuario
+    // * contraseña
+    // * u > 18 para abrir cuenta
+    // * menu para transaccion de usuario 1 a usuario 2
+    // * tarjeta con unico numero
+
     public static void main(String[] args) {
 
     }
